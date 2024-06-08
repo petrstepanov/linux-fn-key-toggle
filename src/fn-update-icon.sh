2b3c7f49ee9b04c37a6ae809060bb3b61c4ec49c
Petr Stepanov <stepanovps@gmail.com>
2024-06-08 13:53:43 -0700

    Update notifications

--- src/fn-update-icon.sh
+++ src/fn-update-icon.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
+# Script updates the Application icon according to the system's FNMODE value
 
+# Obtain FNMODE value 
 FNMODE=`cat /sys/module/hid_apple/parameters/fnmode`
-echo Current FNMODE is $FNMODE
+echo "INFO: FNMODE is ${FNMODE}"
 
-SHORTCUT=~/.local/share/applications/fn-toggle.desktop
+LAUNCHER=~/.local/share/applications/fn-toggle.desktop
 
 # Reflect the icon depending on the curent value of the FNMODE
 if [ $FNMODE = 1 ]; then
-    # Keys act like "special" keys
+    # Keys act like "media" keys
     # Delete line containing "Icon="
-    sed -i "/Icon=/d" $SHORTCUT
+    sed --in-place "/Icon=/d" $LAUNCHER
     # Add line with new icon
-    echo "Icon=fn-media" >> $SHORTCUT
+    echo "Icon=fn-media" >> $LAUNCHER
 else
-    # Keys act like "F#"
+    # Keys act like "function" keys
     # Delete line containing "Icon="
-    sed -i "/Icon=/d" $SHORTCUT
+    sed --in-place "/Icon=/d" $LAUNCHER
     # Add line with new icon
-    echo "Icon=fn-function" >> $SHORTCUT
+    echo "Icon=fn-function" >> $LAUNCHER
 fi
 
 # Update application menu list
 xdg-desktop-menu forceupdate
+
+# Exit success
+exit 0
